ae0384e843fbd77da8eb171489bf19a74b27dd34
Alca259 <alca259@gmail.com>
2022-05-29 23:22:13 +0200

    Insomnia mod, retrieved from UMOD https://umodframework.com/mod?id=15 and converted to QMod

---
+++ Insomnia/MainPatcher.cs
@@ -0,0 +1,34 @@
+﻿using Harmony;
+using System.Reflection;
+using UnityEngine;
+
+namespace Insomnia
+{
+    public class MainPatcher
+    {
+        public static bool NoSleep = true;
+
+        public static void Patch()
+        {
+            //_cfg = Config.GetOptions();
+            var val = HarmonyInstance.Create($"Alca259.graveyardkeeper.Insomnia");
+            val.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(BuffsLogics))]
+        [HarmonyPatch(nameof(BuffsLogics.AddBuff))]
+        public class PatchNoSleep
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(string buff_id)
+            {
+                if (!NoSleep || !buff_id.Contains("buff_tired"))
+                    return true;
+
+                Debug.Log("[Insomnia] buff_tired: buff blocked : success!");
+                MainGame.me.player.ClearTiredness();
+                return false;
+            }
+        }
+    }
+}
